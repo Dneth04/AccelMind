64feae7a1ac00039a366177c178146e2c33307ac
Nethradevi <112483364+Dneth04@users.noreply.github.com>
2024-08-20 09:14:02 +0400

    Update page.js

--- app/sign-in/[[...sign-in]]/page.js
+++ app/sign-in/[[...sign-in]]/page.js
@@ -6,7 +6,7 @@ import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export default function SignUpPage( ){
     return <Container maxWidth="100vw">
-       <AppBar position="static" sx={{ backgroundColor: 'transparent', boxShadow: 'none' }}>
+       <AppBar position="static" sx={{ backgroundColor: 'black', boxShadow: 'none' }}>
   <Toolbar>
   <BoltSharpIcon></BoltSharpIcon>
     <Typography variant="h6" style={{ flexGrow: 1 }}>
