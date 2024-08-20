3708a1df7bea5ae20cbf030a93c3f59987aae0d7
Nethradevi <112483364+Dneth04@users.noreply.github.com>
2024-08-20 09:02:33 +0400

    Update page.js

--- app/sign-in/[[...sign-in]]/page.js
+++ app/sign-in/[[...sign-in]]/page.js
@@ -1,29 +1,25 @@
 "use client"
 import { SignIn } from "@clerk/clerk-react";
 import { Toolbar, Typography,Button, Link, AppBar, Container,Box } from "@mui/material";
+import BoltSharpIcon from '@mui/icons-material/BoltSharp';
 
 export default function SignUpPage( ){
     return <Container maxWidth="100vw">
-        <AppBar position="static" sx={{backgroundColor: '#3f51b3'}}>
-            <Toolbar>
-                <Typography variant = "h6"
-                sx={{
-                    flexGrow: 1,
-                }}>
-                    FlashLearn
-                </Typography>
-                <Button color="inherit">
-                    <Link href="/sign-in" passHref>
-                    Login
-                    </Link>
-                </Button>
-                <Button color="inherit">
-                    <Link href="/sign-up" passHref>
-                    Sign Up
-                    </Link>
-                </Button>
-            </Toolbar>
-        </AppBar>
+       <AppBar position="static" sx={{ backgroundColor: 'transparent', boxShadow: 'none' }}>
+  <Toolbar>
+  <BoltSharpIcon></BoltSharpIcon>
+    <Typography variant="h6" style={{ flexGrow: 1 }}>
+      AccelMind
+    </Typography>
+    <SignedOut>
+      <Button color="inherit" href="/sign-in">Login</Button>
+      <Button color="inherit" href="/sign-up">Sign Up</Button>
+    </SignedOut>
+    <SignedIn>
+      <UserButton />
+    </SignedIn>
+  </Toolbar>
+</AppBar>
 
         <Box
         display="flex"
